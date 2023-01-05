bb6a6dbe5084d7a458a85b8c0041a41ec8cbeccb
l1terally <literallyuriy@gmail.com>
2023-01-05 17:25:12 +0800

    Initial Commit

---
+++ json_ex/json_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_exApp.swift
+//  json_ex
+//
+//  Created by Юрий  Лизунов on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct json_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
