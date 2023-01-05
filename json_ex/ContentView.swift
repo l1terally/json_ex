2c4c945c584042735ad485d8a285175f47f53ee3
l1terally <literallyuriy@gmail.com>
2023-01-05 19:59:12 +0800

    git add json_ex/ContentView.swift

--- json_ex/ContentView.swift
+++ json_ex/ContentView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var posts: [Post] = []
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List(posts) { post in
+                VStack{
+                    Text(post.title)
+                        .fontWeight(.bold)
+                    Text(post.body)
+                }
+            } .onAppear() {
+                Api().getPost { (posts) in
+                    self.posts = posts
+                }
+            }.navigationBarTitle("Posts")
         }
-        .padding()
     }
 }
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+
+class Api {
+    func getPost(completion: @escaping ([Post]) -> ()){
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        else{ return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
+    }
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
